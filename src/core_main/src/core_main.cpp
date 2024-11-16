65f5ff51d07bb461dccb959f871561d54e44046b
smileatl <songlei.lin@qq.com>
2024-11-17 01:56:46 +0800

    first commit

---
+++ src/core_main/src/core_main.cpp
@@ -0,0 +1,13 @@
+﻿// SalCore.cpp : Defines the entry point for the application.
+//
+
+#include "core_main.h"
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello CMake." << endl;
+    return 0;
+}
